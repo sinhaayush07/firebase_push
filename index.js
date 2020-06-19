32e521dd3d7a38411fb3d4d5ad8c9a720faf62fd
Ayush Sinha <toayushofficial@gmail.com>
2020-06-19 17:26:34 +0530

    added bg notif

--- index.js
+++ index.js
@@ -27,7 +27,15 @@ messaging.onMessage(function (payload) {
   console.log('message: ', payload)
 })
 
-messaging.setBackgroundMessageHandler(function (payload) {
-  console.log('backgroundMessage: ', payload)
-})
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
 
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
