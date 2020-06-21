a45fb3a10bf89355321e7b0407901f298335abed
Ayush Sinha <toayushofficial@gmail.com>
2020-06-21 16:39:34 +0530

    added background Handler

--- index.js
+++ index.js
@@ -26,16 +26,3 @@ messaging.requestPermission().then(res => {
 messaging.onMessage(function (payload) {
   console.log('message: ', payload)
 })
-
-messaging.setBackgroundMessageHandler(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
-
-  return self.registration.showNotification(notificationTitle,
-    notificationOptions);
-});
